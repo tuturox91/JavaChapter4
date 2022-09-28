56cd1c9ea1f99c51602c834bda2c57aa76a0a07f
John Doe <tuturox91@gmail.com>
2022-09-28 17:09:54 +0300

    Start

---
+++ src/Emploee.java
@@ -0,0 +1,32 @@
+import java.time.LocalDate;
+
+public class Emploee {
+
+    private final String _name;
+    private double _salary;
+    private final LocalDate _hireDay;
+
+    public Emploee(String name, double salary, int year, int month, int day) {
+        _name = name;
+        _salary = salary;
+        _hireDay = LocalDate.of(year,month,day);
+    }
+
+    public String get_name() {
+        return _name;
+    }
+
+    public double getSalary() {
+        return _salary;
+    }
+
+    public LocalDate getHireDay() {
+        return _hireDay;
+    }
+
+    public void raiseSalary(double byPercent) {
+        double rise = _salary * byPercent /100;
+        _salary+=rise;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Emploee[] workers = new Emploee[3];
+
+        workers[0] = new Emploee("Vladislav", 9300, 2022, 9,28);
+        workers[1] = new Emploee("Anton", 7400, 2022, 3,3);
+        workers[2] = new Emploee("Gordon Freeman", 3, 1998, 11,19);
+
+        for (Emploee people :workers)
+        {
+            people.raiseSalary(10);
+            System.out.println("Name: " + people.get_name() + ", Salary: " + people.getSalary() + ", " + people.getHireDay());
+        }
+
     }
 }
