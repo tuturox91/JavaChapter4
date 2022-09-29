2de71796a5ee5dac205cfc2d7a47a31db620f901
John Doe <tuturox91@gmail.com>
2022-09-29 14:33:52 +0300

    fixed

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Emploee[] workers = new Emploee[3];
 
+        Manager manager = new Manager("Starnger",6600, 2022,9,10, 5);
 
-        workers[0] = new Emploee("Vlad", 9300, 2022, 9,28);
+        workers[0] = manager;
         workers[1] = new Emploee(null, 7400, 2022, 3,3);
         workers[2] = new Emploee("Gordon Freeman", 3, 1998, 11,19);
 
-        Manager manager = new Manager("Starnger",6600, 2022,9,10, 5);
+
 
 
         manager.set_bonus(30);
@@ -17,7 +18,7 @@ public class Main {
 
 
 
-        System.out.println("Name: " + manager.get_name() + ", Salary: " + manager.getSalary() + ", Hair Day:" + manager.getHireDay() + ", Current Bonus:" + manager.get_bonus());
+        //System.out.println("Name: " + manager.get_name() + ", Salary: " + manager.getSalary() + ", Hair Day:" + manager.getHireDay() + ", Current Bonus:" + manager.get_bonus());
 
 
         for (Emploee people :workers)
